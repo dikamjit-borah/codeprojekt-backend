65f2a440b7d24fb56771cceeded0ba4124369587
dikamjit-borah <dikamjitborah.devacc01@gmail.com>
2025-05-24 00:01:24 +0530

    rule engine to categorize spus of products

---
+++ utils/ruleEngine.js
@@ -0,0 +1,50 @@
+const { Engine } = require("json-rules-engine");
+
+class RuleEngine {
+  constructor() {
+    if (!RuleEngine.instance) {
+      this.engine = new Engine();
+      this.operatorsAdded = false;
+      RuleEngine.instance = this;
+    }
+
+    return RuleEngine.instance;
+  }
+
+  getEngine() {
+    if (!this.operatorsAdded) {
+      this.engine.addOperator("includes", (factValue, jsonValue) => {
+        if (typeof factValue !== "string") return false;
+        return factValue.toLowerCase().includes(jsonValue.toLowerCase());
+      });
+      this.operatorsAdded = true;
+    }
+    return this.engine;
+  }
+}
+
+const ruleEngine = new RuleEngine();
+
+async function evaluateRules(factsList, rulesConfig, extractFacts) {
+  const engine = ruleEngine.getEngine();
+
+  // Clear existing rules to avoid duplicates during multiple calls
+  engine.rules = [];
+  rulesConfig.forEach((rule) => engine.addRule(rule));
+
+  const results = [];
+
+  for (const item of factsList) {
+    const facts = extractFacts(item);
+    const { events } = await engine.run(facts);
+
+    results.push({
+      originalItem: item,
+      matchedEvents: events || [],
+    });
+  }
+
+  return results;
+}
+
+module.exports = { evaluateRules };
