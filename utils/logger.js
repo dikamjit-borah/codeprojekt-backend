e19d36f74d834384715831bedf3939d656c7f244
dikamjit-borah <dikamjitborah.devacc01@gmail.com>
2025-08-31 18:20:53 +0530

    update logger.js

--- utils/logger.js
+++ utils/logger.js
@@ -17,22 +17,16 @@ function getCaller() {
   try {
     const err = new Error();
     const stack = err.stack.split('\n');
-    
-    // Stack trace when called from logger.info:
-    // 0: Error
-    // 1: at getCaller (logger.js:...)
-    // 2: at Object.info (logger.js:...)  
-    // 3: at actual caller location <- This is what we want
-    
+
     // Start from index 3 and find the first line that's not in logger.js
     for (let i = 3; i < stack.length; i++) {
       const line = stack[i];
-      
-      if (line && 
-          !line.includes('logger.js') &&
-          !line.includes('node_modules') && 
-          !line.includes('node:internal')) {
-        
+
+      if (line &&
+        !line.includes('logger.js') &&
+        !line.includes('node_modules') &&
+        !line.includes('node:internal')) {
+
         // Extract file path and line number using regex
         const match = line.match(/\(([^)]+)\)/) || line.match(/at\s+([^(]+)/);
         if (match && match[1]) {
@@ -50,65 +44,30 @@ function getCaller() {
   } catch (e) {
     // Ignore errors
   }
-  
-  return 'unknown:unknown';
+
+  return '';
 }
 
-// Create wrapper functions that include caller information
+// Helper function to create log methods
+function createLogMethod(level) {
+  return (msg, ...args) => {
+    const caller = getCaller();
+    if (typeof msg === 'object') {
+      baseLogger[level](msg, ...args);
+    } else {
+      baseLogger[level](`${caller} ${msg}`, ...args);
+    }
+  };
+}
+
+// Create logger with dynamic methods
 const logger = {
-  info: (msg, ...args) => {
-    const caller = getCaller();
-    if (typeof msg === 'object') {
-      baseLogger.info(msg, ...args);
-    } else {
-      baseLogger.info(`${caller} ${msg}`, ...args);
-    }
-  },
-  
-  error: (msg, ...args) => {
-    const caller = getCaller();
-    if (typeof msg === 'object') {
-      baseLogger.error(msg, ...args);
-    } else {
-      baseLogger.error(`${caller} ${msg}`, ...args);
-    }
-  },
-  
-  warn: (msg, ...args) => {
-    const caller = getCaller();
-    if (typeof msg === 'object') {
-      baseLogger.warn(msg, ...args);
-    } else {
-      baseLogger.warn(`${caller} ${msg}`, ...args);
-    }
-  },
-  
-  debug: (msg, ...args) => {
-    const caller = getCaller();
-    if (typeof msg === 'object') {
-      baseLogger.debug(msg, ...args);
-    } else {
-      baseLogger.debug(`${caller} ${msg}`, ...args);
-    }
-  },
-  
-  trace: (msg, ...args) => {
-    const caller = getCaller();
-    if (typeof msg === 'object') {
-      baseLogger.trace(msg, ...args);
-    } else {
-      baseLogger.trace(`${caller} ${msg}`, ...args);
-    }
-  },
-  
-  fatal: (msg, ...args) => {
-    const caller = getCaller();
-    if (typeof msg === 'object') {
-      baseLogger.fatal(msg, ...args);
-    } else {
-      baseLogger.fatal(`${caller} ${msg}`, ...args);
-    }
-  }
+  info: createLogMethod('info'),
+  error: createLogMethod('error'),
+  warn: createLogMethod('warn'),
+  debug: createLogMethod('debug'),
+  trace: createLogMethod('trace'),
+  fatal: createLogMethod('fatal'),
 };
 
 module.exports = logger;
