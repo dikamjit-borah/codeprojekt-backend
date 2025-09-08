90ec8c54b7ffa0b8cfd16b3f0b03dac7a4760856
dikamjit-borah <dikamjitborah.devacc01@gmail.com>
2025-09-08 18:47:33 +0530

    move app configs from env to mongo

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,9 +1,26 @@
 const crypto = require("crypto");
+const mongo = require("../providers/mongo");
+const cache = require("../utils/internalCache");
+
 
 function generateHash(data) {
   return crypto.createHash("sha256").update(JSON.stringify(data)).digest("hex");
 }
 
+async function fetchAppConfigs() {
+  const cacheKey = `configs:app`;
+
+  const cached = cache.getKey(cacheKey);
+  if (cached) {
+    return cached;
+  }
+
+  const configs = await mongo.find("configs-app");
+  cache.setKey(cacheKey, configs, 86400000); // Cache for 24 hours
+  return configs;
+}
+
 module.exports = {
   generateHash,
+  fetchAppConfigs
 };
