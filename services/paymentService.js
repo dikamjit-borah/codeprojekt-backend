9b0b06114aa110898f4bf958c9ebc114e41aaa1f
dikamjit-borah <dikamjitborah.devacc01@gmail.com>
2026-03-08 16:19:38 +0530

    fetch details of spus from db instead of client

--- services/paymentService.js
+++ services/paymentService.js
@@ -17,7 +17,7 @@ const { fetchAppConfigs } = require("../utils/helpers");
 
 const purchaseSPU = async (
   spuId,
-  spuDetails,
+  _spuDetails,
   spuType,
   userDetails,
   playerDetails,
@@ -28,6 +28,32 @@ const purchaseSPU = async (
   const redirectUrlWithTransactionId = `${'https://stage.codeprojekt.shop/transaction-status'}`
 
   try {
+    // Extract spuIds from spuDetails array
+    const spuIds = map(_spuDetails, 'spuId');
+
+    // Fetch matching SPUs from mobilelegends product
+    const result = await db.aggregate("spus-modified", [
+      { $match: { product: "mobilelegends" } },
+      {
+        $project: {
+          categorizedSPUs: {
+            $filter: {
+              input: "$categorizedSPUs",
+              as: "spu",
+              cond: { $in: [{ $toString: "$$spu.id" }, spuIds] }
+            }
+          }
+        }
+      }
+    ]);
+
+    const spuDetails = (result[0]?.categorizedSPUs || []).map(spu => ({
+      ...spu,
+      spuId: spu.id
+    }));
+    if (spuDetails.length === 0) {
+      throw createHttpError(404, `SPU details not found for provided spuIds`);
+    }
     logger.info("Initiating purchase", {
       transactionId,
       spuId,
